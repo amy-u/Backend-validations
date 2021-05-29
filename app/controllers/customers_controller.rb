1dfd7404aa35e96363d46692a4be3ad8d5ad9739
amy-u <amyung.lm@gmail.com>
2021-05-29 09:02:55 -0700

    Assignment 1 and 2

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -88,8 +88,8 @@ rescue_from ActiveRecord::RecordNotFound, with: :catch_not_found
       params.require(:customer).permit(:first_name, :last_name, :phone, :email)
     end
     
-    def catch_not_found
-      Rails.logger.debug("We had a not foung exception")
+    def catch_not_found(e)
+      Rails.logger.debug("We had a not found exception")
       flash.alert = e.to_s
       redirect_to customers_path
     end
